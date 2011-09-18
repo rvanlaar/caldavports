d3a1f2840f72e4adddce7453de928e3b357fec40
Roland van Laar <roland@MiLaptop.(none)>
2011-09-18 23:01:48 +0200

    1) Upgraded calendarserver to be version 3.0. 2) pep08-ify fabfile.

--- fabfile.py
+++ fabfile.py
@@ -6,7 +6,8 @@ from fabric.api import local, lcd
 projects = {'pycalendar': 'http://svn.mulberrymail.com/repos/PyCalendar/'
                           'branches/server-stable@161',
             'calendarserver':  'http://svn.macosforge.org/repository/'
-                               'calendarserver/CalendarServer/trunk@7517'}
+                               'calendarserver/CalendarServer/tags/'
+                               'release/CalendarServer-3.0'}
 
 
 def get_svn_export(project, url):
@@ -14,9 +15,10 @@ def get_svn_export(project, url):
 
     project_dir = os.path.join('/tmp', project)
     local('rm -rf {project_dir}'.format(project_dir=project_dir))
-    local('svn co "{url}" {project_dir}'.format(url=url, 
+    local('svn co "{url}" {project_dir}'.format(url=url,
                                                 project_dir=project_dir))
-    
+
+
 def build_tar(project):
     """Build the tar for the project."""
 
@@ -30,7 +32,7 @@ def build_tar(project):
     local('rm -rf "{new_project_dir}"'.format(new_project_dir=new_project_dir))
     local('svn export "{project_dir}" "{new_project_dir}"'.format(
         project_dir=project_dir, new_project_dir=new_project_dir))
-    with lcd(new_project_dir.replace('(','\(').replace(')','\)')):
+    with lcd(new_project_dir.replace('(', '\(').replace(')', '\)')):
         # Clean up development left overs
         local('rm -f .project')
         local('rm -f .pydevproject')
@@ -40,7 +42,6 @@ def build_tar(project):
           'project_version': project_version}
     local('tar -C /tmp -czvf "{project_tar}" '
           '"{project_version}"'.format(**pt))
-                                       
 
     if sys.platform == 'linux2':
         local('sha256sum "{project_tar}" |'
@@ -56,7 +57,7 @@ def build_tar(project):
         local('ls -l "{project_tar}" |'
               'awk \'{{printf "SIZE (%s) = %s\\n", $9, $5 }}\' >> distinfo'
               ''.format(**pt))
-        
+
 
 def build(project):
     project_url = projects[project]
